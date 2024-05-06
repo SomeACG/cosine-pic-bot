3a60d60d10c8adf7710a070bf105a09f73266325
yusixian <cosine_yu@qq.com>
2024-05-07 02:37:43 +0800

    feat: prisma sqlite

--- src/bot/commands/echo/index.ts
+++ src/bot/commands/echo/index.ts
@@ -1,7 +1,12 @@
+import { prisma } from '@/utils/db';
+import dayjs from 'dayjs';
 import { Telegraf } from 'telegraf';
 
 const echoCommand = Telegraf.command('echo', async (ctx) => {
   const text = ctx.message.text;
   ctx.reply('echo! ' + text);
+  const images = await prisma.image.findMany();
+  console.log('======= images =======\n', images[0]);
+  console.log('======= day =======\n', dayjs(images[0]?.create_time).format('YYYY-MM-DD HH:mm:ss'));
 });
 export default echoCommand;
--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,58 +1,57 @@
 import { ADMIN_CHAT_IDS, BOT_TOKEN } from '@/constants';
+import { prisma } from '@/utils/db';
 import logger from '@/utils/logger';
 import { Telegraf } from 'telegraf';
 import echoCommand from './commands/echo';
-// import listDirsCompose from './commands/listdirs';
-import { authGuard } from './guards/authGuard';
 import postCommand from './commands/post';
+import { authGuard } from './guards/authGuard';
 
 const bot = new Telegraf(BOT_TOKEN);
 
 const commands = [
   { command: 'start', description: '显示欢迎信息～' },
   { command: 'help', description: '显示帮助～' },
-  { command: 'recommend', description: '投稿，形式为 /recommend url #tag1 #tag2' },
   { command: 'echo', description: '显示 Post 预览，形式为 /echo url #tag1 #tag2' },
+  { command: 'recommend', description: '投稿，形式为 /recommend url #tag1 #tag2' },
   { command: 'random', description: '随机图片' },
   { command: 'post', description: '(admin) 发图到频道，形式为 /post url #tag1 #tag2' },
   { command: 'mark_dup', description: '(admin) 标记该图片已被发送过，形式为 /mark_dup url ' },
   { command: 'unmark_dup', description: '(admin) 在频道评论区回复，形式为 /unmark_dup url ' },
 ];
-//? [用户] /start
-bot.start((ctx) => ctx.reply('欢迎，请尽情的享受咱吧～\n输入 /help 查看帮助哦'));
 
-//? [用户] /help
-//? [bot] 列出帮助
+bot.start((ctx) => ctx.reply('欢迎，请尽情的享受咱吧～\n输入 /help 查看帮助哦'));
 bot.help((ctx) => {
   const contents = commands.reduce((acc, command) => {
     return acc + `/${command.command} - ${command.description}\n`;
   }, '');
   return ctx.reply('命令列表：\n' + contents);
 });
-
 bot.use(echoCommand);
 bot.use(Telegraf.optional(authGuard(), postCommand));
-// bot.use(Telegraf.optional(authGuard(), listDirsCompose));
 
 // 设置命令
 bot.telegram.setMyCommands(commands);
 
 // Enable graceful stop
-process.once('SIGINT', () => {
+process.once('SIGINT', async () => {
   logger.info('[SIGINT] 呜呜呜人家要被杀掉惹...');
   ADMIN_CHAT_IDS?.length &&
     ADMIN_CHAT_IDS.forEach((adminId) => {
       bot.telegram.sendMessage(adminId, '[SIGINT] 呜呜呜人家要被杀掉惹...');
     });
   bot.stop('SIGINT');
+  await prisma.$disconnect();
+  process.exit(0); // 正常退出进程
 });
-process.once('SIGTERM', () => {
+process.once('SIGTERM', async () => {
   logger.info('[SIGTERM] 呜呜呜人家要被杀掉惹...');
   ADMIN_CHAT_IDS?.length &&
     ADMIN_CHAT_IDS.forEach((adminId) => {
       bot.telegram.sendMessage(adminId, '[SIGTERM] 呜呜呜人家要被杀掉惹...');
     });
   bot.stop('SIGTERM');
+  await prisma.$disconnect();
+  process.exit(0); // 正常退出进程
 });
 
 export default bot;
